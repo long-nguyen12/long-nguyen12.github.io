958b150787ad0cc33fdd741762ce1b81ca74292a
Nguyen Long <longnguyenhoang.ict@gmail.com>
2026-04-07 15:21:12 +0200

    update footer

--- components/footer.js
+++ components/footer.js
@@ -7,32 +7,18 @@ class Footer extends HTMLElement {
     this.innerHTML = `
     <div class="footer-lemos mt-5">
         <div class="py-3 px-md-5 container-fluid">
-            <div class="row">
-                <div class="col-auto col-md-5 align-middle">
+            <div class="row align-items-center">
+                <div class="col-12 col-md-6">
                     <p class="mb-0" style="color: var(--light-gray);">© 2026 Hoang Long Nguyen</p>
                     <p class="my-0 pt-0"><a class="" href="mailto:longnguyenhoang.ict@gmail.com">longnguyenhoang.ict@gmail.com</a></p>
                 </div>
-                <div class="col-auto col-md-7 text-md-right align-middle">
-                    <p class="mb-0" style="font-weight: 600; color: var(--blue-medium);">
-                        Total visitors: <span id="footer-visitor-count">Loading...</span>
-                    </p>
+                <div class="col-12 col-md-6 text-md-right mt-3 mt-md-0">
+                    <img src="https://hitscounter.dev/api/hit?url=https%3A%2F%2Flong-nguyen12.github.io%2F&label=&icon=display&color=%23140330&message=&style=flat&tz=UTC" alt="Visitor counter">
                 </div>
             </div>
         </div>
     </div>
     `;
-
-    const footerProjectName = "long-nguyen-website";
-    fetch(`https://api.countapi.xyz/hit/${footerProjectName}`)
-      .then((response) => response.json())
-      .then((data) => {
-        const el = this.querySelector("#footer-visitor-count");
-        if (el) el.textContent = data.value.toLocaleString();
-      })
-      .catch(() => {
-        const el = this.querySelector("#footer-visitor-count");
-        if (el) el.textContent = "N/A";
-      });
   }
 }
 
