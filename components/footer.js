718b82c9e94e17abc1e74c17dc81b858588d31dd
Nguyen Long <longnguyenhoang.ict@gmail.com>
2026-04-07 15:15:54 +0200

    update

--- components/footer.js
+++ components/footer.js
@@ -12,11 +12,27 @@ class Footer extends HTMLElement {
                     <p class="mb-0" style="color: var(--light-gray);">© 2026 Hoang Long Nguyen</p>
                     <p class="my-0 pt-0"><a class="" href="mailto:longnguyenhoang.ict@gmail.com">longnguyenhoang.ict@gmail.com</a></p>
                 </div>
+                <div class="col-auto col-md-7 text-md-right align-middle">
+                    <p class="mb-0" style="font-weight: 600; color: var(--blue-medium);">
+                        Total visitors: <span id="footer-visitor-count">Loading...</span>
+                    </p>
+                </div>
             </div>
         </div>
-
     </div>
     `;
+
+    const footerProjectName = "long-nguyen-website";
+    fetch(`https://api.countapi.xyz/hit/${footerProjectName}`)
+      .then((response) => response.json())
+      .then((data) => {
+        const el = this.querySelector("#footer-visitor-count");
+        if (el) el.textContent = data.value.toLocaleString();
+      })
+      .catch(() => {
+        const el = this.querySelector("#footer-visitor-count");
+        if (el) el.textContent = "N/A";
+      });
   }
 }
 
